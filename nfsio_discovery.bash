0ecb3b1d6b86ba16cfcaf870fce2c0fbb21cde9d
Antonio Ettorre <56338190+vot4anto@users.noreply.github.com>
2020-06-08 09:44:51 +0200

    Update and rename nfsio_discovery.sh to nfsio_discovery.bash

--- nfsio_discovery.bash
+++ nfsio_discovery.bash
@@ -1,9 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # ============================================================
-# Author: Dmitry Malinin 
-# E.mail: dmitry@malinin.com
-# Blog: http://zabbix.guru
-# Filename: nfsio_discovery.sh
+# use env instead of bash
 # Modified: 2018-01-10 
 # Description: Discovery NFS mountpoints.
 #
